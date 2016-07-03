391920210fd8cbac27e1aa5e4dfd5e480db1a464
KarinaKulish <karina.kulish1311@gmail.com>
2016-07-03 15:10:56 +0300

    project commit

---
+++ Sorting/app/src/main/java/com/example/dellpc/sorting/IntContainer.java
@@ -0,0 +1,12 @@
+package com.example.dellpc.sorting;
+
+/**
+ * Created by DELL PC on 01.07.2016.
+ */
+public class IntContainer {
+    public int value;
+
+    public IntContainer(int initialValue) {
+        value = initialValue;
+    }
+}
